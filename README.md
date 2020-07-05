eb794c2a8e3d260196ad3f09cfb48448c47a954a
Richard Germaine <rlgerma92@outlook.com>
2020-07-05 16:30:16 -0600

    init

---
+++ README.md
@@ -0,0 +1,5 @@
+## Gatsby headless WordPress e-commerce demo
+
+- Forked from "gatsby-wordpress-starter" and modified to work with woocommerce rest API
+
+- wp backend managed on flywheel
---
+++ gatsby-config.js
@@ -0,0 +1,40 @@
+module.exports = {
+  siteMetadata: {
+    title: 'React + WordPress e-commerce',
+  },
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-sass',
+    {
+      resolve: 'gatsby-source-wordpress',
+      options: {
+        // The base url to your WP site.
+        baseUrl: 'wp-react-demo.flywheelsites.com',
+        // WP.com sites set to true, WP.org set to false
+        hostingWPCOM: false,
+        // The protocol. This can be http or https.
+        protocol: 'https',
+
+        restApiRoutePrefix: 'wp-json',
+        // Use 'Advanced Custom Fields' Wordpress plugin
+        useACF: false,
+        auth: {},
+        // Set to true to debug endpoints on 'gatsby build'
+        verboseOutput: false,
+      },
+    },
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
+    {
+      // Removes unused css rules
+      resolve: 'gatsby-plugin-purgecss',
+      options: {
+        // Activates purging in gatsby develop
+        develop: true,
+        // Purge only the main css file
+        purgeOnly: ['/all.sass'],
+      },
+    }, // must be after other CSS plugins
+    'gatsby-plugin-netlify', // make sure to keep it last in the array
+  ],
+}
